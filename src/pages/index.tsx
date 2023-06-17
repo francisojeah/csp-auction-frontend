42010a92976945b85c51102c4e53c141958b2b5e
francisojeah <ojeahfrancis@gmail.com>
2023-06-17 13:44:04 +0100

    updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import Login from "@/components/login";
+import Login from "@/components/Login";
 import SignUp from "./signup";
 import Homee from "./homee";
 import { getArtWorks } from "../pages/api/sheets";
