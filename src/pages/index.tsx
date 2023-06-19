1ba9ed9a906aedb22d6efc1de9fb27f54a097b96
francisojeah <ojeahfrancis@gmail.com>
2023-06-19 11:00:19 +0100

    updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,9 @@
-import Adminpage from "./adminpage";
 import Homee from "./homee";
-import { useSession, signIn, signOut } from "next-auth/react";
 
-type Props = {
-  items: any;
-};
-
-export default function Home({ items }: Props) {
-  const { data: session } = useSession();
-
+export default function Home() {
   return (
     <main>
       <Homee />
     </main>
   );
 }
-
-
