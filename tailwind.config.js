2f9e2f9da69c8180cdd2657997a8cc4ae76bfa16
francisojeah <ojeahfrancis@gmail.com>
2023-06-22 13:44:34 +0100

    demo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
+// const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  media: false,
   theme: {
     extend: {},
     // screens: {
     //   ...defaultTheme.screens,
     // },
+  },
+  variants: {
+    extend:{}
   },
   plugins: [],
 };
