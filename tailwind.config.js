4ed553de899a6a9103e17a2d41f1bbbe33a03305
francisojeah <ojeahfrancis@gmail.com>
2023-06-22 02:04:42 +0100

    new updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,15 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'background-image': "url('/assets/images/backgroundimage.jpg')",
+        "background-image": "url('/assets/images/backgroundimage.jpg')",
       },
     },
     screens: {
